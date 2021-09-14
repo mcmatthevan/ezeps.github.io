87bdcb31b312c89537f80a4c254abb76bcd5145b
mcmatthevan <mcmatthevan@gmail.com>
2021-09-14 20:26:12 +0200

    mdr

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-    
+       
     function $_GET(param) {
         var vars = {};
         window.location.href.replace( location.hash, '' ).replace( 
@@ -12,8 +12,8 @@
             return vars[param] ? vars[param] : null;	
         }
         return vars;
-    }
-
+    } 
+    
             var map = L.map(
                 "map",
                 {
@@ -24,24 +24,18 @@
                     preferCanvas: false,
                 }
             );
-            
+
             map.on("click",function(e){
                 console.log( e.latlng)
                 window.top.postMessage(e.latlng.lat+','+e.latlng.lng, '*');
                 location.href = IP + "/visualize?posX=" + e.latlng.lat + "&posY=" + e.latlng.lng + "&pattern=" + $_GET("pattern");
             });
-            
 
         
     
-            var tile_layer_396dc129e2504b7b8edfecd5a7aa8a78 = L.tileLayer(
-                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                {"attribution": "Data by \u0026copy; \u003ca href=\"http://openstreetmap.org\"\u003eOpenStreetMap\u003c/a\u003e, under \u003ca href=\"http://www.openstreetmap.org/copyright\"\u003eODbL\u003c/a\u003e.", "detectRetina": false, "maxNativeZoom": 19, "maxZoom": 19, "minZoom": 17, "noWrap": false, "opacity": 1, "subdomains": "abc", "tms": false}
+            var tile_layer_26d874eb9dfc4ac2876c7a34d8e8114a = L.tileLayer(
+                "https://api.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}@2x.png?access_token=sk.eyJ1IjoibHVtYWhlbGwiLCJhIjoiY2t0a2M1dXFlMDFsZjJ1bjJlZXR6anF4byJ9.X-_qJgkBC6ROLU-T91i-6A",
+                {"attribution": "Mapbox", "detectRetina": false, "maxNativeZoom": 23, "maxZoom": 23, "minZoom": 17, "noWrap": false, "opacity": 1, "subdomains": "abc", "tms": false}
             ).addTo(map);
         
     
-            var tile_layer_ef669f6b135247aab5f5fccf985685b7 = L.tileLayer(
-                "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-                {"attribution": "Esri", "detectRetina": false, "maxNativeZoom": 19, "maxZoom": 19, "minZoom": 17, "noWrap": false, "opacity": 0.8, "subdomains": "abc", "tms": false}
-            ).addTo(map);
-        

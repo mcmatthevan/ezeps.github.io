018c8d1392e6b06caf0d2153fe85cd662d49fe63
mcmatthevan <mcmattevan@gmail.com>
2021-09-14 09:54:36 +0200

    ezall

--- add.js
+++ add.js
@@ -12,29 +12,68 @@ function $_GET(param) {
 	}
 	return vars;
 }
+
 $(function(){
-    var current = 0;
+    var current1 = 0, current2 = 0;
     var cord;
-    $("button").click(function(){
-        if (current == 0){
-            current = -1
+    var coordsm;
+window.onmessage = function(ev){
+    current2 = 1;
+    let data = ev.data.split(",");
+    coordsm = [parseFloat(data[0]),parseFloat(data[1])];
+};
+    $("#b2").click(function(){
+        if (current2 == 0){
+            $("#method1").remove();
+            current2 = -1;
+            $("p").html("Cliquez pour déterminer la position.");
+            $("p").after("<iframe src='test.html?pattern="+ $_GET("pattern")+ "'></iframe>");
+        } else if (current2 == 1){
+            let pattern = $_GET("pattern").split(",");
+            for (let i = 0 ; i < pattern.length ; i++){
+                pattern[i] = parseInt(pattern[i]);
+            }
+            $("iframe").remove();
+            $("p").html("Veuillez patienter... <img class='loading_img' src='https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif' alt=''/>");
+            /posX=([0-9\.]+)&posY=([0-9\.]+)/.test($("iframe").attr("src"));
+            $.ajax({
+                type: "POST",
+                url: IP+"/addpos",
+                data: {args:JSON.stringify({
+                    posX: coordsm[0],
+                    posY: coordsm[1],
+                    pattern: pattern
+                })},
+                dataType: "json",
+                success: function (response) {
+                    if (response === "OK"){
+                        location.href = "index.html";
+                    }
+                }
+            });
+        }
+    });
+    $("#b1").click(function(){
+        if (current1 == 0){
+            $("#method2").remove();
+            current1 = -1
             $("p").html("Détermination de la position, veuillez patienter... <img class='loading_img' src='https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif' alt=''/>");
             navigator.geolocation.getCurrentPosition(function(pos){
                 cord = pos.coords;
                 $("p").html("Confirmez la position en cliquant sur OK.");
-                $("p").after("<embed src=\""+IP+"/visualize?posX="+cord.latitude+"&posY="+cord.longitude+"&pattern="+$_GET("pattern")+"\"></embed>");
-                current = 1;
+                $("p").after("<iframe src=\""+IP+"/visualize?posX="+cord.latitude+"&posY="+cord.longitude+"&pattern="+$_GET("pattern")+"\"></iframe>");
+                current1 = 1;
             },function(){
                 $("p").html("Une erreur est survenue.");
             },{
                 enableHighAccuracy: true
             });
-        } else if (current == 1){
+        } else if (current1 == 1){
             let pattern = $_GET("pattern").split(",");
             for (let i = 0 ; i < pattern.length ; i++){
                 pattern[i] = parseInt(pattern[i]);
             }
-            $("embed").remove();
+            $("iframe").remove();
             $("p").html("Veuillez patienter... <img class='loading_img' src='https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif' alt=''/>");
             $.ajax({
                 type: "POST",
--- ip.js
+++ ip.js
@@ -1,4 +1,4 @@
-if (location.protocol === "file:"){
+if (location.protocol === "file:" || /localhost/.test(location.href)){
     var IP = "http://localhost:6700";
 } else {
     var IP = "https://singalong-game.com:6700";

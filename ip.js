a47431f14f220b122488d99bc81eafc5c17eae48
mcmatthevan <65026747+mcmatthevan@users.noreply.github.com>
2022-03-27 21:12:09 +0200

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,5 +1,5 @@
 if (location.protocol === "file:" || /localhost/.test(location.href)){
     var IP = "http://localhost:6700";
 } else {
-    var IP = "https://singalong-game.com:6700";
+    var IP = "https://palgania.ovh:6700";
 }
